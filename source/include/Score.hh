c781bb7f40bdcd08711ae90fac6e810637c3a16f
yoshi-jun <83324691+yoshi-jun@users.noreply.github.com>
2021-09-28 09:57:47 +0900

    Add license statement

--- source/include/Score.hh
+++ source/include/Score.hh
@@ -1,30 +1,44 @@
+/*==============================================================================
+  BSD 2-Clause License
+  Copyright (c) 2021 Junichi Yoshida
+  All rights reserved.
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+  1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+==============================================================================*/
+
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Score.hh
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
+#ifndef Score_h
+#define Score_h 1
 class Score_Edeps
 {
+    private:
+        ~Score_Edeps();
+        
+        Score_Edeps();
+
+        static std::vector<std::vector<std::vector<double>>> Score_edep;
 
     public:
-        ~Score_Edeps(){
-
-            std::ofstream outdata("data.csv");
-
-            for (i=0; i < 61; i++ ){
-
-                for (j=0; j < 61; j++ ){
-                
-                    for (k=0; k < 150; k++ ){
-
-                        outdata << Score_edep.at(i).at(j).at(k);
-                    
-                    }
-                }
-            }
-
-            outdata.close();
         
-        }
+        static auto pointer;
 
         void GetScorerPointer(){
 
@@ -32,23 +46,15 @@ class Score_Edeps
         
         }
 
-        void AddScoreData(i,j,k,data){
+        void AddScoreData();
 
-            Score_edep.at(i).at(j).at(k) = data;
+        static  Score_Edeps& GetInstance(){
+            
+            static Score_Edeps pointer;
 
+            return pointer;
         }
 
-    private:
-
-        Score_Edep(){
-    
-            static pointer = new *ScoreEdep();
-    
-        }
-    
-        static pointer
-        static Score_Edep*;
-        
-        static std::vector<std::vector<std::vector<double>>> Score_edep;
 
 };
+#endif
--- source/src/Score.cc
+++ source/src/Score.cc
@@ -1,9 +1,33 @@
+/*==============================================================================
+  BSD 2-Clause License
+  Copyright (c) 2021 Junichi Yoshida
+  All rights reserved.
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+  1. Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+  2. Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+  OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+==============================================================================*/
+
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Source.cc
 //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-Score_Edeps::Score_Edeps(){
+#include"Score.hh"
 
-    pointer = new *Score_Edeps();
+Score_Edeps::Score_Edeps(){
 
 }
 
@@ -11,13 +35,13 @@ Score_Edeps::~Score_Edeps(){
 
     std::ofstream outdata("data.csv");
 
-    for (i=0; i < 61; i++ ){
+    for (int i = 0; i < 61; i++ ){
 
-        for (j=0; j < 61; j++ ){
+        for (int j = 0; j < 61; j++ ){
         
-            for (k=0; k < 150; k++ ){
+            for (int k = 0; k < 150; k++ ){
 
-                outdata << Score_edep.at(i).at(j).at(k);
+                outdata << Score_edep.at(i).at(j).at(k) << "\n";
             
             }
         }
@@ -27,13 +51,8 @@ Score_Edeps::~Score_Edeps(){
 
 }
 
-void GetScorerPointer(){
-
-    return pointer;
-}
-
 void AddScoreData(x,y,z,data){
 
     Score_edep.at(x).at(y).at(z) = data;
-    
+
 }
